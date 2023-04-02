9ea10a1b4d11a649b838fe519a5159fbb92c468f
ste107 <129275776+ste107@users.noreply.github.com>
2023-04-02 10:20:42 +0200

    Update filosofi.c
    dichiarazione delle costanti e inserimento prototipi necessari per lo svolgimento del problema

--- filosofi.c
+++ filosofi.c
@@ -3,4 +3,25 @@
 #include <unistd.h>
 #include <semaphore.h>
 
-int main(){}
+#define NUM_FIL 5
+#define PENSA 2
+#define AFFAMATO 1
+#define MANGIA 0
+#define SINISTRA (filosofi+4) % NUM_FIL
+#define DESTRA (filosofi+1) % NUM_FIL
+
+int stato[NUM_FIL];
+int fil[NUM_FIL] = {0,1,2,3,4};
+
+sem_t mutex;
+sem_t F[NUM_FIL];
+
+void verifica(int filosofi);
+void prendeForchetta(int filosofi);
+void lasciaForchetta(int filosofi);
+void* filosofo(void* num);
+
+int main(){
+    int i;
+    pthread_t fil_id[NUM_FIL];
+}
