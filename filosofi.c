bed58ad5128d88d6155506e6fbe4a6ce72992127
ste107 <129275776+ste107@users.noreply.github.com>
2023-04-02 11:34:02 +0200

    Update filosofi.c
    creazione del join del thread nel main, implementazione dello schermo iniziale e implementazione della procedura prendeForchetta(int filosofi)

--- filosofi.c
+++ filosofi.c
@@ -17,30 +17,52 @@ int fil[NUM_FIL] = {0,1,2,3,4};
 sem_t mutex;
 sem_t F[NUM_FIL];
 
+void schermoIniziale();
 void verifica(int filosofi);
 void prendeForchetta(int filosofi);
 void lasciaForchetta(int filosofi);
 void* filosofo(void* num);
 
+void prendeForchetta(int filosofi){
+    sem_wait(&mutex);
+    stato[filosofi] = AFFAMATO;
+    printf("Filosofo %d e' affamato\n", filosofi+1);
+    verifica(filosofi);
+    sem_post(&mutex);
+    sem_wait(&F[filosofi]);
+    sleep(1);
+}
+
 void* filosofo(void* num){
     while (true) {
-        int* i = num;
-	    sleep(1);
-		prendeForchetta(*i);
-	    sleep(1);
-		lasciaForchetta(*i);
-	}
+    int* i = num;
+	sleep(1);
+	prendeForchetta(*i);
+	sleep(1);
+	lasciaForchetta(*i);
+    }
 }
 
 int main(){
     int i;
     pthread_t fil_id[NUM_FIL];
     sem_init(&mutex,0,1);
-	for (i = 0; i < NUM_FIL; i++){
-	    sem_init(&F[i],0,0);
+    schermoIniziale();
+    sleep(2);
+    for (i = 0; i < NUM_FIL; i++){
+	sem_init(&F[i],0,0);
     }
     for (i = 0; i < NUM_FIL; i++) {
-	    pthread_create(&fil_id[i],NULL,filosofo, &fil[i]);
-	    printf("Filosofo %d PENSA\n",i+1);
+	 pthread_create(&fil_id[i],NULL,filosofo, &fil[i]);
+	 printf("Filosofo %d PENSA\n",i+1);
     }
+    for (i = 0; i < NUM_FIL; i++){
+	pthread_join(fil_id[i],NULL);
+    }
+}
+
+void schermoIniziale(){
+    printf("+-----------BENVENUTO------------+\n");
+    printf("|   Dining philosophers problem  |\n");
+    printf("+--------------------------------+\n\n");
 }
